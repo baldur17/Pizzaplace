b193002d801258f0be52e355d66b8a1f02649698
Mainyu <34063768+Mainyu@users.noreply.github.com>
2017-12-14 17:31:08 +0000

    Add files via upload

---
+++ VLN1_Pizza/include/ui/DeliveryUI.h
@@ -0,0 +1,17 @@
+#ifndef DELIVERYUI_H
+#define DELIVERYUI_H
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+class DeliveryUI
+{
+    public:
+        void startUI();
+        DeliveryUI();
+        char validate_user_input(char input);
+
+    private:
+};
+
+#endif // DELIVERYUI_H
